82f7abb0d64a99cabdfa3c5de81266d62973d065
VidurSanghi <52296159+VidurSanghi@users.noreply.github.com>
2020-06-10 12:16:04 -0700

    Initial Commit

---
+++ RishiKumarHackathon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RishiKumarHackathon
+//
+//  Created by Vidur Sanghi on 6/10/20.
+//  Copyright © 2020 ManResa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
